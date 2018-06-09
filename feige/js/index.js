ce0bdfdd5c08ebca271ad116339ecef19ea33bff
xciceboy <443068902@qq.com>
2018-06-09 11:20:08 +0800

    1.方案输入框间距；2.底部文字排版和说明修改；3。导航条底部边框；4.合作伙伴鼠标滑过显示 5.服务流程图宽度 6.关于我们的服务优势文字标点

--- feige/js/index.js
+++ feige/js/index.js
@@ -12,6 +12,23 @@
       'top':0,
       'opacity':0
     },500)
-  })
-	
+  });
+
+ //  var strfooter = [];
+ //    strfooter.push('<div class="inner container">');
+ //    strfooter.push('<div class="footer-top">');
+ //    strfooter.push('<h2 class="row">');
+ //    strfooter.push('<div class="col-md-6 fl-center">飞歌，不止于研发</div>');
+ //    strfooter.push('<div class="col-md-6 fl-center">咨询热线：<strong style="color: #019BE3">182-1009-6007</strong></div></h2>');
+ //    strfooter.push('<div class="row">');
+ //    strfooter.push('<div class="col-md-6 fl-center" style="display: inline-block">飞歌通信（北京）科技有限公司</div>');
+ //    strfooter.push('<div class="col-md-6 fl-center" style="display: inline-block">友情链接：');
+ //    strfooter.push('<a href="http://aliyun.com" style="margin-right: 5px;">阿里云 </a>');
+ //    strfooter.push('<a href="http://www.baidu.cn/" style="margin-right: 5px;">百度 </a>');
+ //    strfooter.push('<a href="https://www.qq.com/">腾讯</a></div></div>');
+ //    strfooter.push('<div class="row">');
+ //    strfooter.push('<div class="col-md-6 fl-center" style="display: inline-block">地址：北京市宋家庄庄子商务楼北楼一层</div>');
+ //    strfooter.push('</div></div></div>');
+ //    strfooter.push('<div class="footer_p">Copyright © 飞歌科技 京ICP备18013854号</div>');
+	// $('#footer').html(strfooter.join(''));
 })();
