f565a2eb586351b1109483ef2639b58e343eaf69
xciceboy <443068902@qq.com>
2018-06-19 11:33:47 +0800

    casedetail

--- feige/js/casedetail.js
+++ feige/js/casedetail.js
@@ -1,7 +1,181 @@
 var objdata = {
 	nngdjt:{
 		title:'南宁轨道交通app'
-		,des:'国内知名上市电商，品尚汇拥有三大运营平台：B2C平台品尚汇；B2B平台店省省；进口酒供应链服务平台， 构建了B2C+B2B+供应链服务的全渠道进口酒类平台'
+		,des:'国内知名上市电商，品尚汇拥有三大运营平台：B2C平台品尚汇；B2B平台店省省；进口酒供应链服务平台， 构建了B2C+B2B+供应链服务的全渠道进口酒类平台。'
 		,flag:['品尚汇APP','品尚汇PC站','品尚汇微信']
+		,content:'<img src="image/case/nngdjt1.png" style="width: 100%;max-width: 1000px;" />'
 	}
+	,baobaoshu:{
+		title:'宝宝树-怀孕育儿大型育儿网站社区'
+		,des:'宝宝树是国内专业的育儿社区平台，新墨为宝宝树先后开发宝宝树孕育和小时光及宝宝树美囤妈妈两三款手机应用，让父母们进行有价值的经验分享及育儿学习，来满足多层次、全方位、适应时代进步育儿需求。'
+		,flag:['社区PC站 ','孕育APP','宝宝树微信']
+		,content:'<img src="image/case/baobaoshu1.jpg" style="width: 100%;max-width: 1000px;" />'
+	}
+	,mangguotv:{
+		title:'芒果TV-湖南卫视官方视频客户端'
+		,des:'芒果TV是湖南广播电视台旗下唯一互联网视频平台，独家提供湖南卫视所有栏目高清视频直播点播，并为用户提供各类热门电影、电视剧、综艺、动漫、音乐、娱乐等内容。'
+		,flag:['芒果PC站','芒果TV-APP','芒果TV-小程序','芒果TV微信']
+		,content:'<img src="image/case/mangguotv1.jpg" style="width: 100%;max-width: 1000px;" />'
+	}
+	,aihuishou:{
+		title:'爱回收-高效手机回收'
+		,des:'卖手机，就上爱回收APP！变废为宝，快速卖钱！爱回收由世界银行和京东战略投资，是全国最知名电子产品回收平台，专注于手机、平板电脑、笔记本、数码相机等电子数码产品的回收服务。'
+		,flag:['爱回收小程序','爱回收APP','爱回收PC','爱回收微信']
+		,content:'<img src="image/case/aihuishou.jpg" style="width: 100%;max-width: 1000px;" />'
+	}
+	,wangyi:{
+		title:'网易-一起打造最有态度的产品'
+		,des:'蜗牛读书、网易美学、网易哒哒，网易严选~~新墨与网易4度牵手合作，一起打造最有态度的产品。'
+		,flag:['蜗牛读书APP','网易美学APP','网易哒哒 APP','网易严选']
+		,content:'<img src="image/case/wangyi.jpg" style="width: 100%;max-width: 1000px;" />'
+	}
+	,feiniui:{
+		title:'飞牛网-大润发网上商城'
+		,des:'飞牛网是自营模式的全品类综合零售购物网站。来自大润发的生活必需品专业网站。提供食品、生鲜、清洁、洗护、百货、母婴、家电、时尚、进口等数百万种商品。'
+		,flag:['飞牛PC电商','飞牛APP','飞牛微信']
+		,content:'<img src="image/case/feiniui.jpg" style="width: 100%;max-width: 1000px;" />'
+	}
+	,tibatu:{
+		title:'土巴兔-装修就上土巴兔'
+		,des:'土巴兔成立于2008年，是面向用户的一站式家装服务平台，目前已开通250个城市分站，累计服务1600万中国家庭。在土巴兔，用户可获得免费验房、免费设计与报价、装修质检、家居电商、品质施工、远程监控工地等服务，一站式享受品质家装。'
+		,flag:['土巴兔PC站','土巴兔APP','土巴兔小程序  ']
+		,content:'<img src="image/case/tibatu.jpg" style="width: 100%;max-width: 1000px;" />'
+	}
+	,weijing:{
+		title:'微鲸-专注于家庭娱乐的互联网科技公司'
+		,des:'微鲸科技有限公司是一家专注创造属于新一代家庭娱乐的互联网科技公司，拥有质感非凡的微鲸智能语音电视、让生活大有玩投的微鲸智能语音投影、不止给你好看的微鲸VR，结合先进科技与海量内容的智能平台，大胆挑战，勇于创新，力求打造属于每个家庭的娱乐世界。'
+		,flag:['微鲸PC站','社区PC站','电商PC站','VR APP','微鲸微信']
+		,content:'<img src="image/case/weijing.jpg" style="width: 100%;max-width: 1000px;" />'
+	}
+	,lanxiniu:{
+		title:'蓝犀牛-货运界的滴滴出行'
+		,des:'蓝犀牛是一家提供同城直送服务的互联网公司，用技术手段为客户提供便捷的运输体验，客户可以在线下单立即用车，城区2个小时内送达；同时我们有严格的司机审核标准及培训认证机制使得整个服务得到保证；按照公里数计费模式让价格更实惠并可提前预知费用。'
+		,flag:['蓝犀牛APP','蓝犀牛PC站']
+		,content:'<img src="image/case/lanxiniu1.jpg" style="width: 100%;max-width: 1000px;" /><img src="image/case/lanxiniu2.jpg" style="width: 100%;max-width: 1000px;" /><img src="image/case/lanxiniu3.jpg" style="width: 100%;max-width: 1000px;" />'
+	}
+	,haichuanghui:{
+		title:'海创汇-创业项目、创业服务'
+		,des:'海创汇提供创业项目、创业服务,是海尔集团打造的创业孵化平台,聚合海尔生态圈产业资源及开放社会资源,为创客提供综合的创业孵化服务。'
+		,flag:['海立方PC站']
+		,content:'<img src="image/case/haichuanghui.jpg" style="width: 100%;max-width: 1000px;" />'
+	}
+	,zaixing:{
+		title:'在行-领先的知识技能共享平台'
+		,des:'「在行」致力于用分享经济的理念让每个人的知识技能各尽所能。从现在开始，当你遇到任何领域的问题或是个性化的服务需求，都可以在这里，径直找到一个可以帮助你的人，答疑解惑，出谋划策，定制服务。'
+		,flag:['在行PC站','在行APP ']
+		,content:'<img src="image/case/zaixing.jpg" style="width: 100%;max-width: 1000px;" />'
+	}
+	,xiaoniu:{
+		title:'小牛电动-把电动车当智能硬件来做'
+		,des:'小牛电动由国际知名风险投资机构投资的高科技创新型企业，专注于高品质、智能化、时尚、动感电动车。焕然新生，新增多种实用功能为牛油提供贴心到位的服务。全新功能模块，让小牛管家更好地为您服务。'
+		,flag:['小牛PC官网','小牛管家APP']
+		,content:'<img src="image/case/xiaoniu1.jpg" style="width: 100%;max-width: 1000px;" /><img src="image/case/xiaoniu2.jpg" style="width: 100%;max-width: 1000px;" />'
+	}
+	,jiudiangege:{
+		title:'酒店哥哥-会场搜索、比价和预订平台'
+		,des:'酒店哥哥HotelGG.com是中国最大的会场搜索、比价和预订平台。免费为客户提供全国5万多酒店和特色场地，适合年会、培训会、团队聚会等各种活动的需求。'
+		,flag:['酒店哥哥PC站','酒店哥哥APP']
+		,content:'<img src="image/case/jiudiangege.jpg" style="width: 100%;max-width: 1000px;" />'
+	}
+	,batianzaixian:{
+		title:'8天在线-中国领先校园网上超市'
+		,des:'以ERP多层级后台管理系统，短距离物流跟踪管理系统，总仓分仓管理系统为服务支撑，以线下云物流、云仓储、云服务管理体系作为服务后台。'
+		,flag:['8天在线APP','8天在线PC站']
+		,content:'<img src="image/case/8tianzaixian.jpg" style="width: 100%;max-width: 1000px;" />'
+	}
+	,shandianjiangjia:{
+		title:'闪电降价-品牌折扣商城'
+		,des:'闪电降价，一款限时特惠APP，新用户注册立享500元优惠券，心动不如行动！NIKE，Adidas，LEE等品牌好货每日限时特卖，闪降专区折后无底线再降价，一次帮你省到底！'
+		,flag:['闪电降价APP']
+		,content:'<img src="image/case/shandianjiangjia1.png" style="width: 33%;max-width: 1000px;" /><img src="image/case/shandianjiangjia2.png" style="width: 33%;max-width: 1000px;" /><img src="image/case/shandianjiangjia3.png" style="width: 33%;max-width: 1000px;" />'
+	}
+	,tancheng:{
+		title:'探城美食-精选、私藏美食推荐'
+		,des:'探城是专注于发现与分享的精品美食社区，一款吃货必备的优质美食探店神器，每日精选探文带你发现好餐厅，已开通全国11个热门城市。'
+		,flag:['探城美食小程序','探城美食APP','探城美食PC']
+		,content:'<img src="image/case/tancheng.jpg" style="width: 100%;max-width: 1000px;" />'
+	}
+	,youxin:{
+		title:'有信-实时音视频通讯App'
+		,des:'高清免费语音，只需轻轻按下一键，拨打电话，无需对方安装，你立即连接所有你的朋友。不仅能听到TA的声音更能看到TA的人，让沟通更加真实。'
+		,flag:['有信APP']
+		,content:'<img src="image/case/youxin1.jpg" style="width: 33%;max-width: 1000px;" /><img src="image/case/youxin2.jpg" style="width: 33%;max-width: 1000px;" /><img src="image/case/youxin3.jpg" style="width: 33%;max-width: 1000px;" />'
+	}
+	,yiziwang:{
+		title:'椅子网-为企业加快人才筛选'
+		,des:'椅子网是专注提升大学生就业质量的视频招聘网站,提供应届生全职实习兼职机会,直达复试的视频简历,视频直播校招宣讲会,500强名企招聘,应届生互联网招聘第一品牌。'
+		,flag:['椅子网pc站','椅子网app']
+		,content:'<img src="image/case/yiziwang.jpg" style="width: 100%;max-width: 1000px;" />'
+	}
+	,wantu:{
+		title:'玩途-发现世界新玩法'
+		,des:'出国到了当地，就要用玩途“吃喝玩乐助手”，分分钟搞定各种当地深度服务，省时、省心、省钱！换当地货币嫌麻烦？玩途帮您消费后快捷买单，只需手机就能完成支付，安全，便捷，还打折。'
+		,flag:['玩途PC站','玩途APP']
+		,content:'<img src="image/case/wantu.jpg" style="width: 100%;max-width: 1000px;" />'
+	}
+	,yitiku:{
+		title:'易题库-智能选题组件'
+		,des:'易题库旨在为初高中老师学生减负,为老师提供简单便捷的组卷服务,为学生提供智能测评功能；专注于提高教学效率、教学成果和提升学生成绩的一款革命性的智能题库产品。'
+		,flag:['易题库pc站']
+		,content:'<img src="image/case/yitiku.jpg" style="width: 100%;max-width: 1000px;" />'
+	}
+	,yixinli:{
+		title:'壹心理-大家都觉得准的心里测试'
+		,des:'壹心理成立于2011年7月，是中国最大的心理学平台，心理学第一媒体，推出了心理FM、壹心理、口袋心理测试等心理应用APP及壹心理学院，是一个受大众及心理学人欢迎的心理学科普及服务平台。'
+		,flag:['壹心理PC站','壹心理APP']
+		,content:'<img src="image/case/yixinli.jpg" style="width: 100%;max-width: 1000px;" />'
+	}
+	,qianliao:{
+		title:'千聊-打造专人所属的大型培训网校'
+		,des:'千聊是一个专注于知识分享的平台，通过直播的形式让您直接找到各个领域的专家、老师、达人。现场在线，实时互动，可听课、可购买干货内容，可和学友讨论、可和讲师问答，是一个你私人所属的24小时在线的知识智囊团。'
+		,flag:['千聊PC站','千聊APP','千聊微信  ']
+		,content:'<img src="image/case/qianliao.jpg" style="width: 100%;max-width: 1000px;" />'
+	}
+	,touna:{
+		title:'投哪网-优质理财项目随时关注'
+		,des:'投哪网分别获得广发信德A轮亿元融资和大金重工B轮1.5亿融资，中银粤财战略持股,互联网汽车金融平台。投哪网坚持“小额、分散、抵押、直营”的风控战略，提供专业、便捷、透明的理财产品。'
+		,flag:['投哪网PC站','投哪网PC社区','投哪网APP','投哪网微信']
+		,content:'<img src="image/case/touna.jpg" style="width: 100%;max-width: 1000px;" />'
+	}
+	,gongniu:{
+		title:'公牛炒股-新一代炒股神器'
+		,des:'公牛炒股,一款模拟炒股投资理财APP。查看股票实时行情,买卖股票;免费跟踪牛人;专业的分析师和比出来的炒股牛人,分享观点和交易记录,帮助新手股民进行决策。'
+		,flag:['公牛炒股APP']
+		,content:'<img src="image/case/gongniu1.jpg" style="width: 33%;max-width: 1000px;" /><img src="image/case/gongniu2.jpg" style="width: 33%;max-width: 1000px;" />'
+	}
+}
+
+
+var id = Arg('id');
+var obj = objdata[id];
+if (obj && obj.title) {
+	var title = obj.title;
+	var des = obj.des;
+	var flags = obj.flag;
+	var content = obj.content;
+	$('#detailtitle').html(title);
+	$('#detaildes').html(des);
+	var flagArr = [];
+	for (var i = 0; i < flags.length; i++) {
+		flagArr.push('<span style="margin-right: 5px;">' + flags[i] + '</span>')
+	}
+	$('#detailflag').append(flagArr.join(''));
+	$('#caseDetContent').html(content);
+}else{
+	$('#detailtitle').html('');
+	$('#detaildes').html('');
+	$('#detailflag').append('');
+	$('#caseDetContent').html('');
+}
+
+function Arg(key) {
+    var arr = (location.hash || location.search || '').split('?')[1].split('&')
+    var params = {}
+    for (var i = 0; i < arr.length; i++) {
+        var data = arr[i].split('=')
+        if (data.length === 2) {
+            params[data[0]] = data[1]
+        }
+    }
+    return params[key]
 }
